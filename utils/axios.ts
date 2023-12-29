4b3192bb4a71d60a069d52bde7c13b93248a5011
andy922200-m1 <andy922200@gmail.com>
2023-12-30 02:33:26 +0800

    chore: upgrade package

--- utils/axios.ts
+++ utils/axios.ts
@@ -11,7 +11,7 @@ const service = axiosClient.create({
 
 // request interceptor
 service.interceptors.request.use(
-  (config: AxiosRequestConfig) => {
+  (config) => {
     if (!config.headers) {
       throw new Error("Expected 'config' and 'config.headers' not to be undefined")
     }
